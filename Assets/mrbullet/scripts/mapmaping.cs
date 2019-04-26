0b1700ea117551490ffc9670e8b59b42a6408570
magelon <alfardproject@gmail.com>
2019-04-25 22:26:11 -0700

    c

--- Assets/mrbullet/scripts/mapmaping.cs
+++ Assets/mrbullet/scripts/mapmaping.cs
@@ -4,13 +4,52 @@ using UnityEngine;
 
 public class mapmaping : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Texture2D map;
+    //int[,] mapLayer;
+
+    public GameObject obstical;
+    public GameObject bloon;
+    public GameObject playerIllusion;
+
+    void drawMapLayer()
     {
-        
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                Color pixelColor = map.GetPixel(i, j);
+                if (pixelColor.a == 0)
+                {
+                    //spawn passable cubes 0
+                    //mapLayer[i, j] = 0;
+                }
+                else if (pixelColor == Color.white)
+                {
+                    //spawn block can not pass 1
+                    Instantiate(obstical, new Vector3(i, j, 0), Quaternion.identity);
+                    //mapLayer[i, j] = 1;
+                }
+                else if (pixelColor == Color.red)
+                {
+                    Instantiate(bloon, new Vector3(i, j, 0), Quaternion.identity);
+
+                }
+                else
+                {
+                    //set player on the top layer
+                    playerIllusion.transform.position = new Vector3(i, j, 0);
+                   
+
+                }
+            }
+        }
     }
 
-    // Update is called once per frame
+    void Start()
+    {
+        drawMapLayer();
+    }
+
     void Update()
     {
         
